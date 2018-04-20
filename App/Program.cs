3d4b44c22298d93f974c87941d0624b55099cf24
JensMalm <jensmalm873@gmail.com>
2018-04-20 13:42:22 +0200

    no message

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NGB.Data;
+using NGB.Domain;
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

dd8613d08fa4ce4225f85d5593de6450aa185758
JensMalm <jensmalm873@gmail.com>
2018-04-20 15:56:12 +0200

    Customer Class Added

--- NGB.Domain/Program.cs
+++ NGB.Domain/Program.cs
@@ -2,6 +2,21 @@
 
 namespace NGB.Domain
 {
+    public enum PreferedContactType
+    {
+        Phone,
+        EMail,
+        Letter
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EMail { get; set; }
+        public PreferedContactType PreferedContactType { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)

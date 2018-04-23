106ab4856ab2c4c5d571175a997ca5cb9de87b35
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-04-23 11:53:55 +0200

    Commit validate personname

--- NGB.FrontEnd/Validate.cs
+++ NGB.FrontEnd/Validate.cs
@@ -7,6 +7,8 @@ namespace NGB.FrontEnd
 {
     public enum StringType
     {
+        CompanyName,
+        PersonName,
         Email,
         PhoneNumber
     }
@@ -17,11 +19,17 @@ namespace NGB.FrontEnd
             var isValid = false;
             switch (stringType)
             {
+                case StringType.CompanyName:
+                    isValid = ValidateString(ValidateCompanyName, stringToValidate);
+                    break;
+                case StringType.PersonName:
+                    isValid = ValidateString(ValidatePersonName, stringToValidate);
+                    break;
                 case StringType.Email:
-                    isValid = ValidateEmail(stringToValidate);
+                    isValid = ValidateString(ValidateEmail, stringToValidate);
                     break;
                 case StringType.PhoneNumber:
-                    isValid = ValidatePhoneNumber(stringToValidate);
+                    isValid = ValidateString(ValidatePhoneNumber, stringToValidate);
                     break;
                 default:
                     break;
@@ -29,21 +37,35 @@ namespace NGB.FrontEnd
             return isValid;
         }
 
+        
+        public bool ValidateString(Func<string, bool> validate, string stringToValidate)
+        {
+            if (String.IsNullOrEmpty(stringToValidate))
+                return false;
+            else
+                return validate(stringToValidate);
+        }
+  
+
+        public bool ValidateCompanyName(string input)
+        {
+                return true;
+        }
+
+        public bool ValidatePersonName(string input)
+        {
+           return Regex.IsMatch(input, @"^[a-zA-ZåäöÅÄÖ]+(([',. -][a-zA-ZåäöÅÄÖ ])?[a-zA-ZåäöÅÄÖ]*)*$");
+        }
+
+
         public bool ValidatePhoneNumber(string input)
         {
-            return Regex.IsMatch(input, @"^\d{2,10}-?\d{1,8}$");
+            return Regex.IsMatch(input, @"^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$");
         }
         public bool ValidateEmail(string input)
         {
-            if (String.IsNullOrEmpty(input))
-                return false;
-            else
-            {
-                return Regex.IsMatch(input, @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+            return Regex.IsMatch(input, @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                                             @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
-            }
-
-            
         }
     }
 }
